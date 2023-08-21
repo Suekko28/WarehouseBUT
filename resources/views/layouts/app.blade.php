e213edcc00e1b5bb08ba96ca27008ce1570831e5
Aldi Dharmawan <108856819+Suekko28@users.noreply.github.com>
2023-08-21 13:11:43 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
         <meta content="{{config('app.name')}}" name="description" />
         <meta content="{{config('app.name')}}" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
         <!-- App favicon -->
         <link rel="shortcut icon" href="{{ asset('template/assets') }}/images/favicon.ico">
 
